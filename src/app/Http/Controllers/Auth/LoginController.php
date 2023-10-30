8896e26fc25e9686e4475a50204a1fde9212c7ec
Nakai_Rio <2002bird2002@gmail.com>
2023-10-30 09:59:54 +0900

    :feat: ログイン時のバリデーションを追加

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,5 +37,27 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+    * Handle a login request to the application.
+     *
+    * @param \App\Http\Requests\LoginRequest $request
+    * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+    */
+    public function login(LoginRequest $request)
+    {
+        return $this->traitLogin($request);
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param \App\Http\Requests\LoginRequest $request
+     * @return array
+     */
+    protected function credentials(LoginRequest $request)
+    {
+        return $request->only($this->username(), 'password');
     }
 }
