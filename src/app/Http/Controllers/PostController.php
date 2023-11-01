e1baf5657c1f10e9311fd1c93778e4b839bccca7
Nakai_Rio <2002bird2002@gmail.com>
2023-11-01 15:03:59 +0900

    :change: Serviceの追加による修正

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Http\Requests\PostRequest;
+use App\Services\PostService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\View\View;
 
 class PostController extends Controller
 {
+    /**
+     * The post service instance.
+     *
+     * @var PostService
+     */
+    protected $postService;
+
     /**
      * 投稿画面を表示
      *
@@ -20,6 +27,16 @@ class PostController extends Controller
     }
 
     /**
+     * インスタンスを生成
+     *
+     * @param PostService $postService
+     */
+    public function __construct(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
+
+    /**
      * 投稿を登録
      *
      * @param Postrequest $request
@@ -27,15 +44,14 @@ class PostController extends Controller
      */
     public function createPost(Postrequest $request): RedirectResponse
     {
-        $post = new Post();
-
         $data = [
             'title' => $request->title,
             'post' => $request->post
         ];
-
+        // $date = $request->only(['title', 'post']);
+        
         // タイトルと投稿の登録
-        $post->store($data);
+        $this->postService->create($data);
 
         // 後にポスト一覧に切り替える
         return redirect(route('home'));
